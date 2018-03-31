408e22a174671985f8cfc24ac54c0d6f3d9b6fc3
Gustavo Ingrassia <gustavo.ingrassia@gmail.com>
2018-03-31 11:58:36 -0300

    fix some styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {PureComponent} from 'react';
+
+import logo from './logo.png';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default class App extends PureComponent {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to Pi Home</h1>
+                </header>
+                <p className="App-intro">
+                    Coming soon...
+                </p>
+            </div>
+        );
+    }
 }
-
-export default App;
