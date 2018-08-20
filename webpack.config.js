b6d23306019c06e9e9a9244ecc69019a496ce58a
Gustavo Ingrassia <gustavo.ingrassia@gmail.com>
2018-08-20 11:29:27 -0300

    attempt to fix service worker

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,14 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const webpack = require('webpack');
 
-const getWorkspaceName = () => {
-    const {name} = require('./package.json');
+const {
+    packageName,
+    homepage
+} = require('./package.json');
 
-    return name;
-};
-const workspaceName = getWorkspaceName();
 const getBuildPath = () => path.resolve(__dirname, 'build');
 const getEslintFile = () => path.resolve(__dirname, '.eslintrc.json');
 
@@ -17,7 +18,7 @@ module.exports = (env, argv) => ({
     target: 'web',
     output: {
         path: getBuildPath(),
-        filename: `${workspaceName}.js`,
+        filename: `${packageName}.js`,
     },
     module: {
         rules: [
@@ -82,5 +83,35 @@ module.exports = (env, argv) => ({
             from: 'public/favicon.ico',
             to: getBuildPath(),
         }]),
+        new webpack.EnvironmentPlugin({
+            PUBLIC_URL: homepage,
+        }),
+        new SWPrecacheWebpackPlugin({
+            // By default, a cache-busting query parameter is appended to requests
+            // used to populate the caches, to ensure the responses are fresh.
+            // If a URL is already hashed by Webpack, then there is no concern
+            // about it being stale, and the cache-busting can be skipped.
+            dontCacheBustUrlsMatching: /\.\w{8}\./,
+            filename: 'service-worker.js',
+            logger(message) {
+                if (message.indexOf('Total precache size is') === 0) {
+                    // This message occurs for every build and is a bit too noisy.
+                    return;
+                }
+                if (message.indexOf('Skipping static resource') === 0) {
+                    // This message obscures real errors so we ignore it.
+                    // https://github.com/facebook/create-react-app/issues/2612
+                    return;
+                }
+                console.log(message);
+            },
+            minify: true,
+            // Don't precache sourcemaps (they're large) and build asset manifest:
+            staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+            // `navigateFallback` and `navigateFallbackWhitelist` are disabled by default; see
+            // https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#service-worker-considerations
+            // navigateFallback: publicUrl + '/index.html',
+            // navigateFallbackWhitelist: [/^(?!\/__).*/],
+        }),
     ],
 });
