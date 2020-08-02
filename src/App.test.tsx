c6a5b44c08ad0d842a6b9120eb866eee3c0e6a45
Gustavo Ingrassia <gustavo.ingrassia@gmail.com>
2020-08-01 23:55:48 -0300

    change page basics

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const h1Element = getByText(/Pi Home/i)
+  expect(h1Element).toBeInTheDocument()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from './logo.svg'
+import logo from './logo.png'
 import './App.css'
 
 function App() {
@@ -7,18 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Welcome to Pi Home</h1>
       </header>
+      <p className="App-intro">
+        Coming soon...
+      </p>
     </div>
   )
 }
